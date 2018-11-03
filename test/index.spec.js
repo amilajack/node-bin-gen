7ed3d15339534c0b12e730708835214af73a51e6
Amila Welihinda <amilajack@gmail.com>
2018-11-03 14:38:09 -0700

    add initial boilerplate for tests

---
+++ test/index.spec.js
@@ -0,0 +1,8 @@
+const node = require.resolve('../@amilajack/node');
+const { execSync } = require('child_process');
+
+describe('basic tests', () => {
+    it('should get the node version', () => {
+        execSync('')
+    });
+});
